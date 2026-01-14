54b534a7755942076c2eeb9a5d61f5aa5960f9ba
Labib Abiy <labibabiy07@gmail.com>
2026-01-14 22:25:06 +0700

    Add files via upload

---
+++ Sesi 1/Tugas Sesi 1 phincon Academy.py
@@ -0,0 +1,6 @@
+print("Hi, Welcome to the Python. ")
+user_name = input("What is your name?")
+user_age = input("What is your age?")
+fav_food = input("What is your favorite food?")
+User_data = f"Your name is {user_name}. You are {user_age} years old and {fav_food} is your favourite food."
+print(User_data)
